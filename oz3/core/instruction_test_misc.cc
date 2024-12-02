683579771a57b01ad1e8f7819ed9172597c98cc7
John Pursey <john.pursey@gmail.com>
2024-12-01 22:55:51 -0800

    Add test for WAIT

--- oz3/core/instruction_test_misc.cc
+++ oz3/core/instruction_test_misc.cc
@@ -19,13 +19,52 @@ TEST_F(InstructionTest, NOP_Cycles) {
   ASSERT_TRUE(Init());
   auto& state = GetState();
   state.ResetCore();
+
   for (int i = 0; i < 10; ++i) {
     state.code.AddValue(Encode("NOP"));
   }
+
   Execute(kCpuCoreFetchAndDecodeCycles * 10);
   EXPECT_EQ(GetCycles(), kCpuCoreFetchAndDecodeCycles * 10);
   state.Update();
   EXPECT_EQ(state.ip, 10);
+}
+
+TEST_F(InstructionTest, WAIT_ExactCycles) {
+  ASSERT_TRUE(Init());
+  auto& state = GetState();
+  state.ResetCore();
+
+  state.SetRegisters({{CpuCore::R0, 10}});
+  state.code.AddValue(Encode("WAIT", CpuCore::R0));
+
+  static_assert(kCpuCoreFetchAndDecodeCycles < 5);
+  Execute(5);
+  EXPECT_EQ(state.core.GetState(), CpuCore::State::kWaiting);
+  Execute(5);
+  EXPECT_EQ(GetCycles(), 10);
+  EXPECT_NE(state.core.GetState(), CpuCore::State::kWaiting);
+  state.Update();
+  EXPECT_EQ(state.ip, 1);
+  EXPECT_EQ(state.r0, 0);
+}
+
+TEST_F(InstructionTest, WAIT_OneCycle) {
+  ASSERT_TRUE(Init());
+  auto& state = GetState();
+  state.ResetCore();
+
+  state.SetRegisters({{CpuCore::R0, 1}, {CpuCore::R1, 10}});
+  state.code.AddValue(Encode("WAIT", CpuCore::R0));
+  state.code.AddValue(Encode("WAIT", CpuCore::R1));
+
+  Execute(kCpuCoreFetchAndDecodeCycles + 10);
+  EXPECT_EQ(GetCycles(), kCpuCoreFetchAndDecodeCycles + 10);
+  EXPECT_NE(state.core.GetState(), CpuCore::State::kWaiting);
+  state.Update();
+  EXPECT_EQ(state.ip, 2);
+  EXPECT_EQ(state.r0, kCpuCoreFetchAndDecodeCycles - 1);
+  EXPECT_EQ(state.r1, 0);
 }
 
 }  // namespace
