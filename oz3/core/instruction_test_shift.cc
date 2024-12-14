1e0e0a02665b3b85eb5e14d903c5a4fa404baa52
John Pursey <john.pursey@gmail.com>
2024-12-13 22:09:14 -0800

    Add tests for SRA instruction

--- oz3/core/instruction_test_shift.cc
+++ oz3/core/instruction_test_shift.cc
@@ -500,5 +500,347 @@ TEST_F(InstructionTest, SHR_D_CarryByRegister) {
   EXPECT_EQ(state.st, CpuCore::Z) << "SHR.D D0, R2=33";
 }
 
+TEST_F(InstructionTest, SRA_W_ByValue) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+  uint16_t ips[17] = {};
+  for (int i = 1; i < 17; ++i) {
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R0, "$v")).AddValue(0x4000);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.W", CpuCore::R0, Arg(shift_value)));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+  for (int i = 1; i < 15; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.r0, 0x4000 >> i) << "SRA.W R0, " << i;
+    EXPECT_EQ(state.st, 0) << "SRA.W R0, " << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[15]));
+  EXPECT_EQ(state.r0, 0) << "SRA.W R0, 15";
+  EXPECT_EQ(state.st, CpuCore::Z | CpuCore::C) << "SRA.W R0, 15";
+  ASSERT_TRUE(ExecuteUntilIp(ips[16]));
+  EXPECT_EQ(state.r0, 0) << "SRA.W R0, 16";
+  EXPECT_EQ(state.st, CpuCore::Z) << "SRA.W R0, 16";
+}
+
+TEST_F(InstructionTest, SRA_W_ByRegister) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+  uint16_t ips[18] = {};
+
+  for (int i = 0; i < 18; ++i) {
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R0, "$v")).AddValue(0x4000);
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R1, "$v")).AddValue(i);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.W", CpuCore::R0, {"$r", CpuCore::R1}));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  for (int i = 0; i < 15; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.r0, 0x4000 >> i) << "SRA.W R0, R1=" << i;
+    EXPECT_EQ(state.st, 0) << "SRA.W R0, R1=" << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[15]));
+  EXPECT_EQ(state.r0, 0) << "SRA.W R0, R1=15";
+  EXPECT_EQ(state.st, CpuCore::Z | CpuCore::C) << "SRA.W R0, R1=15";
+  ASSERT_TRUE(ExecuteUntilIp(ips[16]));
+  EXPECT_EQ(state.r0, 0) << "SRA.W R0, R1=16";
+  EXPECT_EQ(state.st, CpuCore::Z) << "SRA.W R0, R1=16";
+  ASSERT_TRUE(ExecuteUntilIp(ips[17]));
+  EXPECT_EQ(state.r0, 0) << "SRA.W R0, R1=17";
+  EXPECT_EQ(state.st, CpuCore::Z) << "SRA.W R0, R1=17";
+}
+
+TEST_F(InstructionTest, SRA_W_SignByValue) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+  uint16_t ips[17] = {};
+
+  for (int i = 1; i < 17; ++i) {
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R0, "$v")).AddValue(0x8000);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.W", CpuCore::R0, Arg(shift_value)));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  for (int i = 1; i < 16; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.r0, 0xFFFF & ~((0x8000 >> i) - 1)) << "SRA.W R0, " << i;
+    EXPECT_EQ(state.st, CpuCore::S) << "SRA.W R0, " << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[16]));
+  EXPECT_EQ(state.r0, 0xFFFF) << "SRA.W R0, 16";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, 16";
+}
+
+TEST_F(InstructionTest, SRA_W_SignByRegister) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[18] = {};
+  for (int i = 0; i < 18; ++i) {
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R0, "$v")).AddValue(0x8000);
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R1, "$v")).AddValue(i);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.W", CpuCore::R0, {"$r", CpuCore::R1}));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  ASSERT_TRUE(ExecuteUntilIp(ips[0]));
+  EXPECT_EQ(state.r0, 0x8000) << "SRA.W R0, R1=0";
+  EXPECT_EQ(state.st, CpuCore::S) << "SRA.W R0, R1=0";
+  for (int i = 1; i < 16; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.r0, 0xFFFF & ~((0x8000 >> i) - 1)) << "SRA.W R0, R1=" << i;
+    EXPECT_EQ(state.st, CpuCore::S) << "SRA.W R0, R1=" << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[16]));
+  EXPECT_EQ(state.r0, 0xFFFF) << "SRA.W R0, R1=16";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, R1=16";
+  ASSERT_TRUE(ExecuteUntilIp(ips[17]));
+  EXPECT_EQ(state.r0, 0xFFFF) << "SRA.W R0, R1=17";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, R1=17";
+}
+
+TEST_F(InstructionTest, SRA_W_CarryByValue) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[17] = {};
+  for (int i = 1; i < 17; ++i) {
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R0, "$v"))
+        .AddValue(0x8000 | (1 << (i - 1)));
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.W", CpuCore::R0, Arg(shift_value)));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  for (int i = 1; i < 16; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.r0, 0xFFFF & ~((0x8000 >> i) - 1)) << "SRA.W R0, " << i;
+    EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, " << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[16]));
+  EXPECT_EQ(state.r0, 0xFFFF) << "SRA.W R0, 16";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, 16";
+}
+
+TEST_F(InstructionTest, SRA_W_CarryByRegister) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[18] = {};
+  for (int i = 0; i < 18; ++i) {
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R0, "$v"))
+        .AddValue(0x8000 | (1 << std::max(0, i - 1)));
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R1, "$v")).AddValue(i);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.W", CpuCore::R0, {"$r", CpuCore::R1}));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  ASSERT_TRUE(ExecuteUntilIp(ips[0]));
+  EXPECT_EQ(state.r0, 0x8001) << "SRA.W R0, R1=0";
+  EXPECT_EQ(state.st, CpuCore::S) << "SRA.W R0, R1=0";
+  for (int i = 1; i < 16; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.r0, 0xFFFF & ~((0x8000 >> i) - 1)) << "SRA.W R0, R1=" << i;
+    EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, R1=" << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[16]));
+  EXPECT_EQ(state.r0, 0xFFFF) << "SRA.W R0, R1=16";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, R1=16";
+  ASSERT_TRUE(ExecuteUntilIp(ips[17]));
+  EXPECT_EQ(state.r0, 0xFFFF) << "SRA.W R0, R1=17";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.W R0, R1=17";
+}
+
+TEST_F(InstructionTest, SRA_D_ByValue) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[33] = {};
+  for (int i = 1; i < 33; ++i) {
+    state.code.AddValue(Encode("MOV.LD", 0, "$V")).AddValue32(0x40000000);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.D", 0, Arg(shift_value)));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  for (int i = 1; i < 31; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.d0(), 0x40000000 >> i) << "SRA.D D0, " << i;
+    EXPECT_EQ(state.st, 0) << "SRA.D D0, " << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[31]));
+  EXPECT_EQ(state.d0(), 0) << "SRA.D D0, 31";
+  EXPECT_EQ(state.st, CpuCore::Z | CpuCore::C) << "SRA.D D0, 31";
+  ASSERT_TRUE(ExecuteUntilIp(ips[32]));
+  EXPECT_EQ(state.d0(), 0) << "SRA.D D0, 32";
+  EXPECT_EQ(state.st, CpuCore::Z) << "SRA.D D0, 32";
+}
+
+TEST_F(InstructionTest, SRA_D_ByRegister) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+  uint16_t ips[34] = {};
+
+  for (int i = 0; i < 34; ++i) {
+    state.code.AddValue(Encode("MOV.LD", 0, "$V")).AddValue32(0x40000000);
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R2, "$v")).AddValue(i);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.D", 0, {"$r", CpuCore::R2}));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  for (int i = 0; i < 31; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.d0(), 0x40000000 >> i) << "SRA.D D0, R2=" << i;
+    EXPECT_EQ(state.st, 0) << "SRA.D D0, R2=" << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[31]));
+  EXPECT_EQ(state.d0(), 0) << "SRA.D D0, R2=31";
+  EXPECT_EQ(state.st, CpuCore::Z | CpuCore::C) << "SRA.D D0, R2=31";
+  ASSERT_TRUE(ExecuteUntilIp(ips[32]));
+  EXPECT_EQ(state.d0(), 0) << "SRA.D D0, R2=32";
+  EXPECT_EQ(state.st, CpuCore::Z) << "SRA.D D0, R2=32";
+  ASSERT_TRUE(ExecuteUntilIp(ips[33]));
+  EXPECT_EQ(state.d0(), 0) << "SRA.D D0, R2=33";
+  EXPECT_EQ(state.st, CpuCore::Z) << "SRA.D D0, R2=33";
+}
+
+TEST_F(InstructionTest, SRA_D_SignByValue) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[33] = {};
+  for (int i = 1; i < 33; ++i) {
+    state.code.AddValue(Encode("MOV.LD", 0, "$V")).AddValue32(0x80000000);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.D", 0, Arg(shift_value)));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  for (int i = 1; i < 32; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.d0(), 0xFFFFFFFF & ~((0x80000000 >> i) - 1))
+        << "SRA.D D0, " << i;
+    EXPECT_EQ(state.st, CpuCore::S) << "SRA.D D0, " << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[32]));
+  EXPECT_EQ(state.d0(), 0xFFFFFFFF) << "SRA.D D0, 32";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, 32";
+}
+
+TEST_F(InstructionTest, SRA_D_SignByRegister) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[34] = {};
+  for (int i = 0; i < 34; ++i) {
+    state.code.AddValue(Encode("MOV.LD", 0, "$V")).AddValue32(0x80000000);
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R2, "$v")).AddValue(i);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.D", 0, {"$r", CpuCore::R2}));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  ASSERT_TRUE(ExecuteUntilIp(ips[0]));
+  EXPECT_EQ(state.d0(), 0x80000000) << "SRA.D D0, R2=0";
+  EXPECT_EQ(state.st, CpuCore::S) << "SRA.D D0, R2=0";
+  for (int i = 1; i < 32; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.d0(), 0xFFFFFFFF & ~((0x80000000 >> i) - 1))
+        << "SRA.D D0, R2=" << i;
+    EXPECT_EQ(state.st, CpuCore::S) << "SRA.D D0, R2=" << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[32]));
+  EXPECT_EQ(state.d0(), 0xFFFFFFFF) << "SRA.D D0, R2=32";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, R2=32";
+  ASSERT_TRUE(ExecuteUntilIp(ips[33]));
+  EXPECT_EQ(state.d0(), 0xFFFFFFFF) << "SRA.D D0, R2=33";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, R2=33";
+}
+
+TEST_F(InstructionTest, SRA_D_CarryByValue) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[33] = {};
+  for (int i = 1; i < 33; ++i) {
+    state.code.AddValue(Encode("MOV.LD", 0, "$V"))
+        .AddValue32(0x80000000 | (1 << (i - 1)));
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.D", 0, Arg(shift_value)));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  for (int i = 1; i < 32; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.d0(), 0xFFFFFFFF & ~((0x80000000 >> i) - 1))
+        << "SRA.D D0, " << i;
+    EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, " << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[32]));
+  EXPECT_EQ(state.d0(), 0xFFFFFFFF) << "SRA.D D0, 32";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, 32";
+}
+
+TEST_F(InstructionTest, SRA_D_CarryByRegister) {
+  ASSERT_TRUE(InitAndReset());
+  auto& state = GetState();
+  state.SetRegisters({{CpuCore::ST, 0}});
+
+  uint16_t ips[34] = {};
+  for (int i = 0; i < 34; ++i) {
+    state.code.AddValue(Encode("MOV.LD", 0, "$V"))
+        .AddValue32(0x80000000 | (1 << std::max(0, i - 1)));
+    state.code.AddValue(Encode("MOV.LW", CpuCore::R2, "$v")).AddValue(i);
+    const std::string shift_value = absl::StrCat(i);
+    state.code.AddValue(Encode("SRA.D", 0, {"$r", CpuCore::R2}));
+    ips[i] = state.code.AddNopGetAddress();
+  }
+  state.code.AddValue(Encode("HALT"));
+
+  ASSERT_TRUE(ExecuteUntilIp(ips[0]));
+  EXPECT_EQ(state.d0(), 0x80000001) << "SRA.D D0, R2=0";
+  EXPECT_EQ(state.st, CpuCore::S) << "SRA.D D0, R2=0";
+  for (int i = 1; i < 32; ++i) {
+    ASSERT_TRUE(ExecuteUntilIp(ips[i]));
+    EXPECT_EQ(state.d0(), 0xFFFFFFFF & ~((0x80000000 >> i) - 1))
+        << "SRA.D D0, R2=" << i;
+    EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, R2=" << i;
+  }
+  ASSERT_TRUE(ExecuteUntilIp(ips[32]));
+  EXPECT_EQ(state.d0(), 0xFFFFFFFF) << "SRA.D D0, R2=32";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, R2=32";
+  ASSERT_TRUE(ExecuteUntilIp(ips[33]));
+  EXPECT_EQ(state.d0(), 0xFFFFFFFF) << "SRA.D D0, R2=33";
+  EXPECT_EQ(state.st, CpuCore::S | CpuCore::C) << "SRA.D D0, R2=33";
+}
+
 }  // namespace
 }  // namespace oz3
